4551ad97ef2089e70b1256014b4edd5200b35bcd
RodrigoCaya <rodrigo@cayazaya.cl>
2024-01-20 13:09:13 -0300

    adding start-test to pipeline

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -2,12 +2,6 @@ describe('Render the app', () => {
   it('should render the app', () => {
     cy.visit('http://localhost:3000');
     cy.contains('Welcome to Acme.');
-  });
-});
-
-describe('Render the app', () => {
-  it('should render the app', () => {
-    cy.visit('http://localhost:3000');
     cy.contains('Welcome to Acmexd.');
   });
 });
