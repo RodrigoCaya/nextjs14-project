370ec4d3d4690a655cb2e30e45cb9ebf6910baf3
RodrigoCaya <rodrigo@cayazaya.cl>
2024-01-20 13:17:39 -0300

    deleting one test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -2,6 +2,5 @@ describe('Render the app', () => {
   it('should render the app', () => {
     cy.visit('http://localhost:3000');
     cy.contains('Welcome to Acme.');
-    cy.contains('Welcome to Acmexd.');
   });
 });
